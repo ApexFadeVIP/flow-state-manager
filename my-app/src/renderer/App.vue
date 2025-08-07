350b2ee40acfa7743d1da6ea45262b9b7e509b86
Raghav Khurana <rkhurana@kth.se>
2025-08-07 20:52:49 +0200

    Fix: Rebase for fixing file structure

--- my-app/src/renderer/App.vue
+++ my-app/src/renderer/App.vue
@@ -1,6 +1,24 @@
+<script setup>
+import CameraFeed from './components/CameraFeed.vue'
+</script>
+
 <template>
-  <div id="app">
-    <h1>Flow State Manager</h1>
-    <p>Welcome to the Flow State Manager application!</p>
-  </div>
+  <main id="app-container">
+    <h1>Facial Expression Detection</h1>
+    <CameraFeed />
+  </main>
 </template>
+
+<style scoped>
+#app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2em;
+}
+
+h1 {
+  margin-bottom: 1.5em;
+}
+</style>
